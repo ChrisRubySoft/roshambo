f00977ca7b0fef24f2f78cb91ce6e2a1c8c2b410
ChrisRubySoft <316782558+ChrisRubySoft@users.noreply.github.com>
2026-09-01 10:40:56 -0400

    Set up a simple RPS game that goes up to three rounds

--- game.js
+++ game.js
@@ -1,45 +1,55 @@
 //Christian Ruby RoShamBo the game
 
-function getComputerChoice(){
+//variables
+let humanScore = 0;
+let botScore = 0;
 
-let botChoice = Math.floor(Math.random() * 3);
-//returns an integer number
-console.log("Bot picks: " + botChoice)
-return botChoice;
+for (let roundNum = 0; roundNum < 3; roundNum++) {
+  console.log(`--- Round ${roundNum + 1} ---`);
+  playRound();
+}
+
+announceWinner();
+
+function getComputerChoice() {
+  let botChoice = Math.floor(Math.random() * 3);
+  console.log("Bot picks: " + botChoice);
+  return botChoice;
 }
 
 function getHumanChoice() {
   let humanChoice = Number(prompt("Pick a num 0-2"));
-  console.log("Human picks: " + humanChoice)
+  console.log("Human picks: " + humanChoice);
   return humanChoice;
 }
-let humanSelection = getHumanChoice();
-let botSelection = getComputerChoice();
 
-//let humanScore = 0;
-//let botScore = 0;
-//let gameStart = false;
-//let gameOver = false;
-//let gameWon = false;
-//let gameLoss = false;
-//let roundStart = false;
-//let roundAmount = playerChoice
-
-function round(){
-const result = (humanSelection - botSelection + 3) % 3;
-
-if (result === 0) {
-  console.log("Tie");
-} else if (result ===1){
-  console.log("Human Wins");
-  humanScore = humanScore + 1;
-} else {
-  console.log("Human Loses");
-  botScore = botScore + 1;
-}
-}
-
-round();
-//while (gameStart == true) {
+function playRound() {
   
-//}
+  let humanSelection = getHumanChoice();
+  let botSelection = getComputerChoice();
+  
+  const result = (humanSelection - botSelection + 3) % 3;
+
+  if (result === 0) {
+    console.log("Tie");
+  } else if (result === 1) {
+    console.log("Human Wins the round");
+    humanScore++;
+  } else {
+    console.log("Human Loses the round");
+    botScore++; 
+  }
+  
+  console.log(`Current Score -> Human: ${humanScore} | Bot: ${botScore}\n`);
+}
+
+function announceWinner() {
+  console.log("--- FINAL RESULT ---");
+  if (humanScore > botScore) {
+    console.log("Human wins the game!");
+  } else if (botScore > humanScore) {
+    console.log("Bot wins the game!");
+  } else {
+    console.log("The game is a tie!");
+  }
+}
